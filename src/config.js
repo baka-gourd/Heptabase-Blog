2ba5625931c90b6ece828aaf0ee5d5c1eda41711
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2023-05-20 17:32:24 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,8 @@ const CONFIG = {
     'pages': {
         'Home': '48355387-1f00-4330-825c-3db7118546bc',
         'Activity':'activity', // 花园活跃状态页面
-        'About': 'f7dcdf7d-f120-44f5-b3bf-b5d66cf3aa26'
+        'About': 'f7dcdf7d-f120-44f5-b3bf-b5d66cf3aa26',
+        'Link': 'f96c0fef-7301-4697-9244-98e4a8064fe1',
     }
 }
 
